8fb0704b3361a51977d7b255424ef098155d78dc
Jawad Amin <59015763+jawadaminkp@users.noreply.github.com>
2021-07-24 14:54:32 +0500

    files uploaded
    html and javascript files uploaded

---
+++ stopWatchLogic.js
@@ -0,0 +1,79 @@
+const startButton = document.querySelector('[data-action="start"]');
+const stopButton = document.querySelector('[data-action="stop"]');
+const resetButton = document.querySelector('[data-action="reset"]');
+const minutes = document.querySelector('.minutes');
+const seconds = document.querySelector('.seconds');
+
+let timerTime = 00;
+let isRunning = false;
+let interval;
+
+
+
+function startTimer(){
+    if(isRunning) return;
+
+    isRunning = true;
+    interval = setInterval(incrementTimer,1000);
+}
+
+function stopTimer(){
+    if(!isRunning) return;
+
+    isRunning = false;
+    clearInterval(interval);
+
+}
+
+function resetTimer(){
+    stopTimer();
+
+    timerTime = 0;
+
+    minutes.innerText = '00'
+    seconds.innerText = '00'
+
+
+}
+
+
+function incrementTimer(){
+        timerTime++;
+    
+        const numOfMinutes = Math.floor(timerTime / 60);
+        const numOfSeconds = timerTime % 60;
+    
+        console.log({numOfMinutes, numOfSeconds});
+    
+    
+        seconds.innerText = pad(numOfSeconds);
+        minutes.innerText = pad(numOfMinutes);
+
+}
+
+
+
+function pad(number){
+
+    return (number < 10 ) ? '0' + number : number; 
+
+
+    // if (number < 10) {
+    //     return '0' + number;
+    // } else {
+    //     return number;
+    // }
+
+}
+
+
+
+
+
+//evenListeners
+
+startButton.addEventListener('click',startTimer);
+stopButton.addEventListener('click',stopTimer);
+resetButton.addEventListener('click',resetTimer);
+
+
